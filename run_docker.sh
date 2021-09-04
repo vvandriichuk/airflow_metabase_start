3e7658e4893edb952f5fa73dcc22babee4620482
Viktor Andriichuk <vandriichuk@provectus.com>
2021-09-04 11:53:55 +0300

    Fix some troubles with run docker-composes

--- run_docker.sh
+++ run_docker.sh
@@ -1,11 +1,11 @@
 echo killing old docker processes
-docker-compose rm -fs
+docker-compose -f airflow/docker-compose.yml rm -fs
+docker-compose -f postgres_collect_data/docker-compose.yml rm -fs
+docker-compose -f metabase/docker-compose.yml rm -fs
 
 echo building docker containers
-docker-compose -f airflow/docker-compose.yml up --build
+docker-compose -f airflow/docker-compose.yml up -d --build
+docker-compose -f postgres_collect_data/docker-compose.yml up -d --buil
+docker-compose -f metabase/docker-compose.yml up -d --build
 
-echo building docker containers
-docker-compose -f metabase/docker-compose.yml up --build
 
-echo building docker containers
-docker-compose -f postgres_collect_data/docker-compose.yml up --build
